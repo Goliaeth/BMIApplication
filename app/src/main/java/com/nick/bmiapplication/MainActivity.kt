f8b9ad7135f4b65ac92f341d41eef641c220f1bc
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-09-13 16:58:54 +0500

    add JetPack View binding

--- app/src/main/java/com/nick/bmiapplication/MainActivity.kt
+++ app/src/main/java/com/nick/bmiapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.nick.bmiapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nick.bmiapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+
     }
 }
