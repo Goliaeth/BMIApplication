29340d64bec2b9f8a97565adcb049f6de2048e06
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-09-13 17:12:55 +0500

    add logic of BMI calculate

--- app/src/main/java/com/nick/bmiapplication/MainActivity.kt
+++ app/src/main/java/com/nick/bmiapplication/MainActivity.kt
@@ -14,7 +14,24 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        binding.calculateButton.setOnClickListener {
+            val weight = binding.weightEditText.text.toString().toFloat()
+            val height = binding.heightEditText.text.toString().toFloat()
 
+            val myBMI = weight / (height * height)
+
+            binding.bmiTextView.text = myBMI.toString()
+
+            if (myBMI < 18.5) {
+                binding.bmiImageView.setImageResource(R.drawable.underweight)
+            } else if (myBMI >= 18.5 && myBMI < 24.9) {
+                binding.bmiImageView.setImageResource(R.drawable.healthy)
+            } else if (myBMI >= 24.9 && myBMI < 29.9) {
+                binding.bmiImageView.setImageResource(R.drawable.overweight)
+            } else if (myBMI >= 29.9) {
+                binding.bmiImageView.setImageResource(R.drawable.obesity)
+            }
+        }
 
     }
 }
